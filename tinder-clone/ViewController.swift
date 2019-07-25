a14e0f40f5dc2053d589b380f6865fae9ef9e5f7
Yash <shelatkaryash@gmail.com>
2019-07-25 21:27:48 +1000

    Home Screen Views

--- tinder-clone/ViewController.swift
+++ tinder-clone/ViewController.swift
@@ -12,7 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    
+        let greyView = UIView()
+        greyView.backgroundColor = .gray
+        
+        let subViews =  [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subViews)
+        topStackView.distribution = .fillEqually
+        
+    //    let redView = UIView()
+    //  topStackView.backgroundColor = .red
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView,blueView,yellowView])
+    // stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+        stackView.fillSuperview()
+        
+        
+        
     }
 
 
