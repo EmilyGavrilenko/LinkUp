7f86b517d75b70d351e1b0b5875b99afcc8c3875
Yash <shelatkaryash@gmail.com>
2019-07-25 17:28:39 +1000

    Initial Commit

---
+++ tinder-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tinder-clone
+//
+//  Created by yash Shelatkar on 25/7/19.
+//  Copyright © 2019 yash Shelatkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
