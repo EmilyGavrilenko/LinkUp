b20a8ab4e346fb72a2ba90f664b00c57cbe5203a
Yash <shelatkaryash@gmail.com>
2019-07-26 20:25:33 +1000

    Gestures Swipe Left and right done

--- tinder-clone/Views/CardView.swift
+++ tinder-clone/Views/CardView.swift
@@ -12,6 +12,10 @@ class CardView: UIView {
 
     fileprivate let imageView = UIImageView(image:#imageLiteral(resourceName: "lady5c"))
     
+    // Configurations
+    
+    fileprivate let threshold: CGFloat = 100
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -32,7 +36,7 @@ class CardView: UIView {
         case .changed:
             handleChanged(gesture)
         case .ended:
-           handleEnded()
+           handleEnded(gesture)
         default:
             ()
         }
@@ -40,15 +44,31 @@ class CardView: UIView {
     }
     
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer){
+        
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        // rotation
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
     }
     
-    fileprivate func handleEnded(){
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer){
+      //  let thereshold:CGFloat = 100
+        let shouldDismissCardRight = gesture.translation(in: nil).x > threshold
+        let shouldDismissCardLeft = gesture.translation(in: nil).x < threshold * -1
+        
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-            self.transform = .identity
+            if shouldDismissCardRight {
+                self.frame = CGRect(x: 1000, y: 0, width: self.frame.width, height: self.frame.height)
+            } else if shouldDismissCardLeft {
+                self.frame = CGRect(x: -1000, y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
+            }
         }) { (_) in
-            
+            self.transform = .identity
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
     }
     
@@ -57,3 +77,6 @@ class CardView: UIView {
     }
     
 }
+
+
+ 
