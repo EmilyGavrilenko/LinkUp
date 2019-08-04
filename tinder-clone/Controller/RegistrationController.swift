ab4ebbbf126972db459484bfef157d46ebf89e8d
Yash <shelatkaryash@gmail.com>
2019-08-04 12:09:38 +1000

    Keyboard height || Tap gestures added

--- tinder-clone/Controller/RegistrationController.swift
+++ tinder-clone/Controller/RegistrationController.swift
@@ -64,22 +64,62 @@ class RegistrationController: UIViewController {
         super.viewDidLoad()
         
         setupGradientLayer()
+        setUpLayout()
+        setupNotificationsObserver()
+        setupTapGesture()
+
         
-        let stackView = UIStackView(arrangedSubviews: [
-            selectPhotoButton,
-            fullNameTextField,
-            emailTextField,
-            passwordTextField,
-            registerButton
-            ])
-        
-        view.addSubview(stackView)
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-       
     }
+    
+    // MARK:- Private
+    
+    
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        
+        
+        self.view.endEditing(true) // dismiss keyboard
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.transform = .identity
+        })
+        
+      //
+    }
+    
+    fileprivate func setupNotificationsObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHeight), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self) // you will have retain cycle
+    }
+    
+    @objc fileprivate func handleKeyboardHeight() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.transform = .identity
+        })
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+       
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return}
+        let keyboardFrame = value.cgRectValue
+        print(keyboardFrame)
+       
+        // figure how tall gap is from bottom of the screen
+        
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        print(bottomSpace)
+        
+        let difference = keyboardFrame.height - bottomSpace
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
+    }
+    
     
     fileprivate func setupGradientLayer() {
         let gradientLayer = CAGradientLayer()
@@ -89,6 +129,24 @@ class RegistrationController: UIViewController {
         gradientLayer.locations = [0,1]
         view.layer.addSublayer(gradientLayer)
         gradientLayer.frame = view.bounds
+    }
+    
+    lazy var stackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton,
+        fullNameTextField,
+        emailTextField,
+        passwordTextField,
+        registerButton
+        ])
+    
+    fileprivate func setUpLayout() {
+        
+        
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 }
